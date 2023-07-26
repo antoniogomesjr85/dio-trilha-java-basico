ac886930c663055c5782d6b30b977ff9e50c4981
consultorgomes <86512119+consultorgomes@users.noreply.github.com>
2023-07-26 18:13:32 -0300

    Desafio Sintaxe finalizado

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,43 @@
-public class App {
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+            //TODO: criar variaveis,  importar classe scanner
+            //Exibir as perguntas para usuarios 
+            //com Scanner pegar os valores digitados
+            //exibir os valores no terminal
+        Scanner  scanner = new Scanner(System.in).useLocale(Locale.US);  
+        int numero;
+        String agencia;
+        String nomeCliente;
+        String sobrenomeCliente;
+        double saldo;
+
+        System.out.println("Por favor digite o numero da conta:");
+        numero = scanner.nextInt();
+
+        System.out.println("Por favor digitar a Agencia:");
+        agencia = scanner.next();
+
+        System.out.println("Por favor digite seu primeiro nome:");
+        nomeCliente = scanner.next();
+        
+        System.out.println("Por favor digite seu sobrenome:");
+        sobrenomeCliente = scanner.next();
+        
+        System.out.println("Por favor digite o Saldo:");
+        saldo = scanner.nextDouble();
+
+        System.out.println("Olá "+ nomeCliente + " "+ sobrenomeCliente+", obrigado por criar uma conta em nosso banco, sua agência é: "+ agencia + ", sua conta é: "+ numero + " e seu saldo é: " + saldo + " já disponivel para saque.");
+        
+
+
+
+
+
+        
+        
+        
     }
 }
