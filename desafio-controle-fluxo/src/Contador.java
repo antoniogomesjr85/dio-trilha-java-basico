37c56afd5d3623467820b42b945a00f950b68bee
consultorgomes <86512119+consultorgomes@users.noreply.github.com>
2023-07-27 22:40:10 -0300

    Entregando desafio contador

--- desafio-controle-fluxo/src/Contador.java
+++ desafio-controle-fluxo/src/Contador.java
@@ -1,5 +1,41 @@
-public class App {
+import java.util.Locale;
+import java.util.Scanner;
+
+
+public class Contador {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        receberParamentro();
+
     }
+
+
+    static void receberParamentro(){
+        Scanner  scanner = new Scanner(System.in).useLocale(Locale.US);
+        System.out.println("Digite o Primeiro Parâmetro");
+        int paramentroUm = scanner.nextInt();
+        System.out.println("Digite o Segundo Parâmetro");
+        int paramentroDois = scanner.nextInt(); 
+        
+        try {
+
+            contar(paramentroUm, paramentroDois);
+            
+        } catch (ParametrosInvalidosException e) {
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+            receberParamentro();
+            
+        }
+    }
+
+    static void contar(int parametroUm, int parametroDois)throws ParametrosInvalidosException{
+        if (parametroUm >= parametroDois)
+            throw new ParametrosInvalidosException();
+        
+        int contagem = parametroDois - parametroUm;
+        
+            for(int i=0; i<contagem; i++){
+            System.out.println("Imprimindo o número " + (i+1));
+            }
+    }
+
 }
